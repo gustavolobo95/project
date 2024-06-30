0158646a84f74b301550b659e1456b3ed700772f
Gustavo Lobo <gustavo.lobo95@gmail.com>
2024-06-30 20:17:42 -0300

    Adding dependency injection

--- src/main/java/com/lobo/sistema/service/MessageService.java
+++ src/main/java/com/lobo/sistema/service/MessageService.java
@@ -1,7 +1,13 @@
 package com.lobo.sistema.service;
 
+import com.lobo.sistema.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
 }
--- src/main/java/com/lobo/sistema/service/UserService.java
+++ src/main/java/com/lobo/sistema/service/UserService.java
@@ -1,7 +1,12 @@
 package com.lobo.sistema.service;
 
+import com.lobo.sistema.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
 }
