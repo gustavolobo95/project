b848afc74accc41dd07558e7f1f58f9ac5def9c4
Gustavo Lobo <gustavo.lobo95@gmail.com>
2024-07-29 20:40:37 -0300

    Creating user services methods

--- src/main/java/com/lobo/sistema/service/UserService.java
+++ src/main/java/com/lobo/sistema/service/UserService.java
@@ -1,12 +1,33 @@
 package com.lobo.sistema.service;
 
+import com.lobo.sistema.dto.UserDTO;
+import com.lobo.sistema.model.User;
 import com.lobo.sistema.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User save(UserDTO userDTO) {
+        User entity = new User(
+                userDTO.getUsername(),
+                userDTO.getEmail(),
+                passwordEncoder.encode(userDTO.getPassword())
+        );
+
+        return userRepository.save(entity);
+    }
 }
