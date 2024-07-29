457b1db506f9bee4f547a0bfaa53f6cb6d8a535e
Gustavo Lobo <gustavo.lobo95@gmail.com>
2024-07-29 20:41:07 -0300

    Refactoring User endpoints

--- src/main/java/com/lobo/sistema/controller/UserController.java
+++ src/main/java/com/lobo/sistema/controller/UserController.java
@@ -1,8 +1,16 @@
 package com.lobo.sistema.controller;
 
+import com.lobo.sistema.dto.UserDTO;
+import com.lobo.sistema.model.User;
 import com.lobo.sistema.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -10,4 +18,21 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @PostMapping("/createUser")
+    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO) {
+       User response = userService.save(userDTO);
+
+       if(response != null)
+           return new ResponseEntity<>(HttpStatus.CREATED);
+
+       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/getUsers")
+    public @ResponseBody List<User> getUsers() {
+        List<User> users = userService.getAllUsers();
+        if(users != null && !users.isEmpty())
+            return users;
+        return new ArrayList<>();
+    }
 }
